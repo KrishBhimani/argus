7da87eeb5b99bdf0f5e2f482e127139940fd9b1d
KrishBhimani <erkrishbhimani@gmail.com>
2026-09-02 14:43:52 +0530

    feat: pillars, tour, privacy, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { Nav } from './sections/Nav';
 import { Hero } from './sections/Hero';
+import { Pillars } from './sections/Pillars';
+import { Tour } from './sections/Tour';
+import { Privacy } from './sections/Privacy';
+import { Footer } from './sections/Footer';
 
 export function App() {
   return (
@@ -7,7 +11,11 @@ export function App() {
       <Nav />
       <main>
         <Hero />
+        <Pillars />
+        <Tour />
+        <Privacy />
       </main>
+      <Footer />
     </>
   );
 }
